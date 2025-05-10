c26edebd9e04ec5ae4b129b0d02aab298ce6a051
wenzhuo4657 <14783149521@163.com>
2025-05-11 06:50:40 +0800

    feat: 动态网关组件封装

---
+++ gateway-common/src/main/java/org/wenzhuo4657/test/gateway/config/MyRouteDefinitionConfigProperties.java
@@ -0,0 +1,14 @@
+package org.wenzhuo4657.test.gateway.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.wenzhuo4657.test.gateway.bean.MyRouteDefinition;
+
+import java.util.Map;
+
+@Data
+@ConfigurationProperties(prefix = "dynamic.gateway.route", ignoreInvalidFields = true)
+public class MyRouteDefinitionConfigProperties {
+    public Map<String, MyRouteDefinition> routes;
+}
